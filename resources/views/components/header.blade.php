2991efd31b10199ffe328055a88c0de38ae5e106
Ali-Faqy <alifaqi68@gmail.com>
2026-06-17 11:38:11 +0300

    initial commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,21 @@
+<nav class="nav-bar">
+    <div class="logo-section">
+        <img src="{{ asset('images/logo.png') }}" alt="logo">
+    </div>
+    <div class="nav-links">
+        <ul>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Our Portfolio</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Packages</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="social-links">
+            <a class='social-link' href="#"><i class="fa-brands fa-facebook-f"></i></a>
+            <a class='social-link' href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a class='social-link' href="#"><i class="fa-brands fa-x-twitter"></i></a>
+            <a class='social-link' href="#"><i class="fa-brands fa-linkedin"></i></a>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    @vite('resources/css/app.css')
+    @vite('resources/css/components/header.css')
+</head>
+<body>
+    <x-header></x-header>
+</body>
+</html>
