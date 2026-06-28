075156c282789151845d658308fd47591c6c7881
Ali-Faqy <alifaqi68@gmail.com>
2026-06-28 11:31:02 +0300

    v8

--- resources/views/components/payment-plan.blade.php
+++ resources/views/components/payment-plan.blade.php
@@ -1,72 +1,50 @@
 @php
     $plans = [
-    [
-        'title' => 'Basic',
-        'price' => '399',
-        'features' => [
-            'Quality performance',
-            'Free custom domain',
-            'Best hosting ever'
-        ]
-    ],
-    [
-        'title' => 'Advanced',
-        'price' => '499',
-        'features' => [
-            'Quality performance',
-            'Free custom domain',
-            'Best hosting ever',
-            'Outstanding support',
-        ]
-    ],
-    [
-        'title' => 'Premium',
-        'price' => '599',
-        'features' => [
-            'Quality performance',
-            'Free custom domain',
-            'Best hosting ever',
-            'Outstanding support',
-            'web Development',
-        ]
-    ]
-];
+        [
+            'title' => 'Basic',
+            'price' => '399',
+            'features' => ['Quality performance', 'Free custom domain', 'Best hosting ever'],
+        ],
+        [
+            'title' => 'Advanced',
+            'price' => '499',
+            'features' => ['Quality performance', 'Free custom domain', 'Best hosting ever', 'Outstanding support'],
+        ],
+        [
+            'title' => 'Premium',
+            'price' => '599',
+            'features' => [
+                'Quality performance',
+                'Free custom domain',
+                'Best hosting ever',
+                'Outstanding support',
+                'web Development',
+            ],
+        ],
+    ];
 @endphp
-<section class="section-secondary p-y-50">
+<section class="section-secondary">
     <div class="container">
-        <div class="row">
-            <div class="col-12 justify-center">
-                <div class="section-sub-title gradient">
-                    <div></div>
-                    Pricing Plan
-                    <div></div>
-                </div>
+        <div class="section-head align-items-center">
+            <div class="section-subtitle">
+                <div></div>
+                Pricing Plan
+                <div></div>
+            </div>
+            <div class="section-title">
+                Pick the perfect Pricing plan
+            </div>
+            <div class="section-description text-align-center">
+                We help ambitious businesses like yours generate more profits by building awareness,
+                driving web traffic, connecting with customers growing.
             </div>
         </div>
-        <div class="row">
-            <div class="col-12 justify-center element-spacing">
-                <div class="section-title color-primary">
-                    Pick the perfect Pricing plan
-                </div>
+        <div class="section-content">
+            <div class="row">
+                @foreach ($plans as $plan)
+                    <x-plan-card :plan="$plan"></x-plan-card>
+                @endforeach
             </div>
-        </div>
-        <div class="row">
-            <div class="col-2">
-            </div>
-            <div class="col justify-center element-spacing">
-                <div class="section-description color-gray text-center">
-                    We help ambitious businesses like yours generate more profits by building awareness,
-                    driving web traffic, connecting with customers growing.
-                </div>
-            </div>
-            <div class="col-2">
-            </div>
-        </div>
-        <div class="row plan-cards">
-            @foreach ($plans as $plan)
-                <x-plan-card :plan="$plan"></x-plan-card>
-            @endforeach
-
         </div>
     </div>
 </section>
--- resources/views/components/plan-card.blade.php
+++ resources/views/components/plan-card.blade.php
@@ -1,16 +1,16 @@
-<div class="col-4">
-                <div class="plan-card element-spacing">
-                    <div class="plan-info">
-                        <div class="plan-type">{{ $plan['title'] }}</div>
-                        <div class="plan-price element-spacing">${{ $plan['price'] }}<span>/Per month</span></div>
-                        <ul class="plan-features element-spacing">
-                            @foreach ($plan['features'] as $feature)
-                            <li class="element-spacing">{{ $feature }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    <div>
-                        <button class="pay-btn outline-btn">Get Started</button>
-                    </div>
-                </div>
-            </div>
+<div class="col-4 d-flex">
+    <div class="plan-card">
+        <div class="plan-info">
+            <div class="plan-type">{{ $plan['title'] }}</div>
+            <div class="plan-price">${{ $plan['price'] }}<span>/Per month</span></div>
+            <ul class="plan-features">
+                @foreach ($plan['features'] as $feature)
+                    <li>{{ $feature }}</li>
+                @endforeach
+            </ul>
+        </div>
+        <div>
+            <button class="pay-btn outline-btn">Get Started</button>
+        </div>
+    </div>
+</div>
